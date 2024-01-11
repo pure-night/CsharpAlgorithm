224611249bc079408733477dcd8c2e31576e599d
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:24:51 +0900

    Add: 두 수의 차

--- Subtraction.cs
+++ Subtraction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-class Subtraction
+﻿class Subtraction
 {
     public int Solution(int num1, int num2)
     {
