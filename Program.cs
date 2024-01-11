cb18e0f9c499266c1336fdc94f5d393f60c111f1
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:20:56 +0900

    Add: 두 수의 차

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var temp = new Subtraction().Solution(5, 4);
+        Console.WriteLine(temp);
+    }
+}
---
+++ Subtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Subtraction
+{
+    public int Solution(int num1, int num2)
+    {
+        int answer = num1 - num2;
+
+        return answer;
+    }
+}
+
