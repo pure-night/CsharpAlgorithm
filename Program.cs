4d616b2e5fe736e2410a3076b72b838e53a1f722
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:32:28 +0900

    Add: 나이 출력

--- Program.cs
+++ Program.cs
@@ -9,5 +9,7 @@ class Program
         Console.WriteLine(new Multiplication().Solution(5, 4));
 
         Console.WriteLine(new Division().Solution(10, 4));
+
+        Console.WriteLine(new YearOfBirth().Solution(40));
     }
 }
---
+++ YearOfBirth.cs
@@ -0,0 +1,11 @@
+﻿class YearOfBirth
+{
+    public int currentYear = 2022;
+
+    public int Solution(int age)
+    {
+        int answer = currentYear - age + 1;
+
+        return answer;
+    }
+}
