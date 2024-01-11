32221deb8ff35e85cb16675ec2fd322444dfa89e
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:28:00 +0900

    Add: 몫 구하기

--- Program.cs
+++ Program.cs
@@ -7,5 +7,7 @@ class Program
         Console.WriteLine(new Subtraction().Solution(5, 4));
 
         Console.WriteLine(new Multiplication().Solution(5, 4));
+
+        Console.WriteLine(new Division().Solution(10, 4));
     }
 }
