2d617eaa93986ace08294af407c4099515b5e05f
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:25:06 +0900

    Add: 두 수의 곱

---
+++ Multiplication.cs
@@ -0,0 +1,10 @@
+﻿class Multiplication
+{
+    public int Solution(int num1, int num2)
+    {
+        int answer = num1 * num2;
+
+        return answer;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var temp = new Subtraction().Solution(5, 4);
-        Console.WriteLine(temp);
+        Console.WriteLine(new Subtraction().Solution(5, 4));
+
+        Console.WriteLine(new Multiplication().Solution(5, 4));
     }
 }
