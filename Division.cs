775c6c46fcd1af0ae71e6d9673c1ea5358c5f9d6
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-11 09:41:37 +0900

    Add: 두 수의 나눗셈

--- Division.cs
+++ Division.cs
@@ -1,8 +1,15 @@
 ﻿class Division
 {
-    public int Solution(int num1, int num2)
+    public int Solution1(int num1, int num2)
     {
         int answer = num1 / num2;
+
+        return answer;
+    }
+
+    public int Solution2(int num1, int num2)
+    {
+        int answer = 1000 * num1 / num2;
 
         return answer;
     }
--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ class Program
 
         Console.WriteLine(new Multiplication().Solution(5, 4));
 
-        Console.WriteLine(new Division().Solution(10, 4));
+        Console.WriteLine(new Division().Solution1(10, 4));
 
         Console.WriteLine(new YearOfBirth().Solution(40));
 
         Console.WriteLine(new Comparison().Solution(40, 41));
 
         Console.WriteLine(new Addition().Solution(40, 41));
+
+        Console.WriteLine(new Division().Solution2(10, 4));
     }
 }
