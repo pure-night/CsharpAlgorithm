bb438ca51e082958b3b3c2a9b17767c8ba598807
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-12 09:33:39 +0900

    Add: 약수의 합

---
+++ Sources/Level1/SumOfDivisors.cs
@@ -0,0 +1,15 @@
+﻿class SumOfDivisors
+{
+    public int Solution(int n)
+    {
+        int answer = 0;
+
+        for (var i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+                answer += i;
+        }
+
+        return answer;
+    }
+}
