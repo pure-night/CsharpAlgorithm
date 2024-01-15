be2040f4b8d26bd78881fcaf111f9d1ec3051c66
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-15 09:17:10 +0900

    Add: 정수 제곱근 판별

---
+++ Sources/Level1/SquareDiscrimination.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class SquareDiscrimination
+{
+    public long Solution(long n)
+    {
+        var x = Math.Sqrt(n);
+
+        if (x != ((int)x))
+            return -1;
+
+        var answer = (long) Math.Pow(x + 1, 2);
+        return answer;
+    }
+}
