2169fe93365948667debf0e23901b42dac6aa09d
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-16 09:41:07 +0900

    Add: 나누어 떨어지는 숫자 배열

--- Sources/Level1/ArrayDivisor.cs
+++ Sources/Level1/ArrayDivisor.cs
@@ -4,6 +4,23 @@ class ArrayDivisor
 {
     public int[] Solution(int[] arr, int divisor)
     {
-        int[] answer = { };
+        int[] answer = new int[arr.Length];
+        int index = 0;
+        for(var i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % divisor == 0)
+                answer[index++] = arr[i];
+        }
+
+        if (index != 0)
+        {
+            Array.Resize(ref answer, index);
+            Array.Sort(answer);
+            return answer;
+        }
+        else
+        {
+            return new int[]{ -1 };
+        }
     }
 }
