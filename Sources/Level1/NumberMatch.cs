1981c82fc71dc8dbbcdb6b51aa009b4e7352933d
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-18 14:09:11 +0900

    Add: 숫자 짝꿍

---
+++ Sources/Level1/NumberMatch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+class NumberMatch
+{
+    public string Solution(string X, string Y)
+    {
+        var nums = new int[10];
+        var answer = new List<char>();
+
+        foreach (var c in Y)
+            nums[c - '0'] += 1;
+
+        foreach(var c in X)
+        {
+            if (nums[c - '0'] > 0)
+            {
+                answer.Add(c);
+                nums[c - '0'] -= 1;
+            }
+        }
+        if (answer.Count == 0)
+            return "-1";
+
+        answer.Sort();
+        answer.Reverse();
+        if (answer[0] == '0')
+            return "0";
+
+        return string.Concat(answer);
+    }
+}
