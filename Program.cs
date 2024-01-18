6f22f62600af1891f3dbdd488f0e955d83041db5
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-18 09:17:57 +0900

    Add: 내적

--- Program.cs
+++ Program.cs
@@ -73,6 +73,6 @@ class Program
 
         Console.WriteLine(new PrintWaterMelon().Solution(5));
 
-        Console.WriteLine(new PrintWaterMelon().Solution(new int[] { 1, 4, 5, 7 }, new int[] { 5, 8, 2, 4 }));
+        Console.WriteLine(new DotProduct().Solution(new int[] { 1, 4, 5, 7 }, new int[] { 5, 8, 2, 4 }));
     }
 }
