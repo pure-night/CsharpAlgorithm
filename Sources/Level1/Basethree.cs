5d60a69136cfde22c43c4656fa888a1160afe15a
pure-night <58593218+pure-night@users.noreply.github.com>
2024-01-23 09:47:21 +0900

    Add: 3진법 뒤집기

---
+++ Sources/Level1/Basethree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Basethree
+{
+    public int Solution(int n)
+    {
+        int answer = 0;
+        var list = new List<int>();
+
+        while (n > 0)
+        {
+            list.Add(n % 3);
+            n /= 3;
+        }
+
+        list.Reverse();
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            answer += (int)(list[i] * Math.Pow(3, i));
+        }
+
+        return answer;
+    }
+}
