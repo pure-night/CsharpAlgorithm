2b4fa8e0fdfd7b8260b7169f91f5b821b493f373
pure-night <58593218+pure-night@users.noreply.github.com>
2024-03-18 09:09:40 +0900

    Add: 햄버거 만들기

---
+++ Sources/Level1/MakeHamburger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+class MakeHamburger
+{
+    public int Solution(int[] ingredient)
+    {
+        int answer = 0;
+        List<int> checker = new List<int>();
+        var iterator = 1;
+
+        foreach(var i in ingredient)
+        {
+
+        }
+
+        for(var i = 0; i < ingredient.Length; i++)
+        {
+            if (checker.Contains(i))
+            {
+                i += 3;
+                continue;
+            }
+
+            if (ingredient[i] == 1)
+            {
+                if (iterator == 4)
+                {
+                    answer++;
+                    checker.Add(i);
+                    checker.Add(i - 1);
+                    checker.Add(i - 2);
+                    checker.Add(i - 3);
+                    i -= 5;
+                    iterator = 1;
+                }
+                else
+                {
+                    iterator = 2;
+                }
+            }
+            else if (ingredient[i] == iterator)
+            {
+                iterator++;
+            }
+            else
+            {
+                iterator = 1;
+            }
+        }
+
+        return answer;
+    }
+}
